7633e3c654e6e219ebae60c650a7ab559543fb79
TuanDV <vdoan@tibco.com>
2020-06-25 16:51:13 +0700

    Init new repo

---
+++ testcases/com/wordpress/post/Post_01_CreateAndViewPost.java
@@ -0,0 +1,20 @@
+package com.wordpress.post;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Post_01_CreateAndViewPost {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void Validate_01_() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
